98c9931b509143f47ce6c3344e860e9120ff5431
martun <martun.davitavyan@mail.ru>
2026-09-13 19:28:18 +0400

    [KAN-2]  Added user deletion, photo update, profile info update.

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -80,7 +80,7 @@ def change_password() -> Response | str:
 @profile_bp.route("/delete_account", methods=["POST"])
 @login_required
 def delete_account() -> str:
-    path = f"avatars/{current_user.id}"
+    path = current_user.profile_picture
     current_user.delete_user()
     supabase_client.delete_profile_picture(path=path)
     flash("Your account has been deleted successfully.", "success")
